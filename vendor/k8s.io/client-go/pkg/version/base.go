b580da3763cac81cba9e0d706daa44627b8436c9
parchk <565625207@qq.com>
2020-04-08 15:28:00 +0800

    fix vendor

---
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "17"
+	gitVersion   = "v1.17.4-k3s1"
+	gitCommit    = "8d3a931508ca468176c6c8a5632beee4d7bb2528"
+	gitTreeState = "clean"
+	buildDate = "2020-03-13T18:06:33Z"
+)
